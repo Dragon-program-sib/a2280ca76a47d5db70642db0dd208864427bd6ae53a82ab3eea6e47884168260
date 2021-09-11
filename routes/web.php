106bc6f741b4f3318cd7bfc49a5de0cb59f4f43a
Dragon <dragon.program.sib@gmail.com>
2021-09-11 19:39:12 +0000

    Update web.php.

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,46 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/name/{name}', function(string $name) {
+    return "Hello, {$name}!";
+});
+
+
+$text = 'Привет, Мир!';
+$title = 'Laravel - Главная';
+$content = 'Установка Laravel и окружения, прошла успешно. :)';
+
+Route::get('/page', function () use ($text, $title, $content) {
+    return <<<php
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>$title</title>
+        </head>
+
+        <body>
+            <h1>$text</h1>
+            <p>Здесь могла быть информация.</p>
+            <p>Много информации.)</p>
+            <p>$content</p>
+        </body>
+
+        </html>
+    php;
+});
+
+
 // Admin.
 Route::group(['prefix' =>'admin', 'as' => 'admin.'], function() {
     Route::resource('categories', AdminCategoryController::class);
     Route::resource('news', AdminNewsController::class);
 });
+
 
 // News.
 Route::get('/news', [NewsController::class, 'index'])

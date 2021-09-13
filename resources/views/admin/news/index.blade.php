97cad9b7c60a990ab14cd8a02c1e3c72fadeefae
Dragon <dragon.program.sib@gmail.com>
2021-09-14 02:24:01 +0700

    Update admin panel.

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -1,15 +1,39 @@
 @extends('layouts.admin')
-<h2>Панель администратора</h2>
-
-@if(empty($newsList))
-    <h3>Новости отсутствуют.</h3>
-@else
-    @foreach($newsList as $news)
-        <div>
-            <h2><a href="{{ route('news.show', ['id' => $news['id']]) }}">{{ $news['title'] }}</a></h2>
-            <p>{!! $news['description'] !!}</p>
+@section('content')
+<div class="col-md-8">
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Список новостей</h1>
+        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Добавить новость</a>
         </div>
-    @endforeach
-@endif
-
+    <div class="table-responsive">
+        <table class="table table-bordered">
+            <thead>
+                <th>#ID</th>
+                <th>Заголовок</th>
+                <th>Описание</th>
+                <th>Дата добавления</th>
+                <th>Управление</th>
+            </thead>
+            @forelse($newsList as $news)
+                <tbody>
+                    <tr>
+                        <td>{{ $news['id'] }}</td>
+                        <td>{{ $news['title'] }}</td>
+                        <td>{!! $news['description'] !!}</td>
+                        <td>{{ now()->format('d-n-Y H:i') }}</td>
+                        <td>
+                            <a href="">Редактировать</a>
+                            &nbsp;
+                            <a href="">Удалить</a>
+                        </td>
+                    </tr>
+                </tbody>
+            @empty
+                <h3>Новости отсутствуют!</h3>   
+            @endforelse
+        </table>
+    </div>
+</div>
 {{-- Комментарий. --}}
+@endsection
